dc3fbb0214558131c7cb1ab6ff6b363b0cf4c250
ohtomi <ohtomi.kenichi@gmail.com>
2015-05-25 22:30:31 +0900

    jsxhintの警告を抑えるために未使用変数を除去した

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -4,7 +4,6 @@
 'use strict';
 
 var React = require('react');
-var ProductStore = require('./store/ProductStore');
 var FilterableProductTable = require('./component/FilterableProductTable');
 
 React.render(<FilterableProductTable />, document.getElementById('content'));
